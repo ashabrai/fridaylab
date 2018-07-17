1c6cb0b1537800664c9ca7596144834853557343
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-07-17 06:40:00 -0700

    fixed the multiply error, changed the string to be exactly what was required

--- app.js
+++ app.js
@@ -32,12 +32,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
+//'The product of 5 and 9 is 45.')
+
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 var total = a * b;
 var arr = [];
 arr.push(total);
-var msg = 'The multiply of ' + a + ' and ' + b + ' is ' + total + '.';
+var msg = 'The product of ' + a + ' and ' + b + ' is ' + total + '.';
 arr.push(msg);
 
 return arr;
